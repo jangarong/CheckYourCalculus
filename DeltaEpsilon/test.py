894b80b26de84e43b4e75121d9e3f47e08f7951d
Jan <42362229+jangarong@users.noreply.github.com>
2020-05-14 17:36:32 -0400

    compares via f(x) - g(x) = 0 instead of f(x)=g(x)

--- DeltaEpsilon/test.py
+++ DeltaEpsilon/test.py
@@ -1,17 +1,22 @@
 from DeltaEpsilon.proof import DEProof
 
 # test case 1
+print("PROOF 1:")
 pf = DEProof("\\lim_{x \\to 2} (x + 3) = 5")
 pf.insert_equation("= |x - 2|")
-pf.insert_equation("= \\delta")
+pf.insert_equation("< \\delta")
 pf.let_delta_be("\\epsilon")
 pf.insert_equation("= \\epsilon")
 pf.print_all_equations()
 
 # test case 2
+print("PROOF 2:")
 pf = DEProof("\\lim_{x \\to 4} 2(x + 3) = 14")
 pf.insert_equation("= |2x + 6 - 14|")
 pf.insert_equation("= |2x - 8|")
-pf.insert_equation("= |2(x - 4)|")  # problem here
 pf.insert_equation("= 2|x - 4|")
+pf.insert_equation("< 2\\delta")
+pf.let_delta_be("\\frac{\\epsilon}{2}")
+pf.insert_equation("= 2\\frac{\\epsilon}{2}")
+pf.insert_equation("= \\epsilon")
 pf.print_all_equations()
