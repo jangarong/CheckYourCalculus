f54be2e1ddf2eac014477bd34852841e4c671941
Jan <42362229+jangarong@users.noreply.github.com>
2020-05-14 22:00:00 -0400

    added one-sided limit proofs

--- DeltaEpsilon/test.py
+++ DeltaEpsilon/test.py
@@ -1,6 +1,6 @@
 from DeltaEpsilon.proof import DEProof
 
-# test case 1
+# test case 1 - straight proof
 print("PROOF 1:")
 pf = DEProof("\\lim_{x \\to 2} (x + 3) = 5")
 pf.insert_equation("= |x - 2|")
@@ -9,8 +9,8 @@ pf.let_delta_be("\\epsilon")
 pf.insert_equation("= \\epsilon")
 pf.print_all_equations()
 
-# test case 2
-print("PROOF 2:")
+# test case 2 - delta with a multiple
+print("\nPROOF 2:")
 pf = DEProof("\\lim_{x \\to 4} 2(x + 3) = 14")
 pf.insert_equation("= |2x + 6 - 14|")
 pf.insert_equation("= |2x - 8|")
@@ -20,3 +20,21 @@ pf.let_delta_be("\\frac{\\epsilon}{2}")
 pf.insert_equation("= 2\\frac{\\epsilon}{2}")
 pf.insert_equation("= \\epsilon")
 pf.print_all_equations()
+
+# test case 3 - one sided limit
+print("\nPROOF 3:")
+pf = DEProof("\\lim_{x \\to 4^-} 2(x + 3) = 14")
+pf.insert_equation("= -(2x + 6 - 14)")
+pf.insert_equation("= -(2x - 8)")
+pf.insert_equation("= -2x + 8)")
+pf.insert_equation("= 2(4 - x)")
+pf.insert_equation("< 2\\delta")
+pf.let_delta_be("\\frac{\\epsilon}{2}")
+pf.insert_equation("= 2\\frac{\\epsilon}{2}")
+pf.insert_equation("= \\epsilon")
+pf.print_all_equations()
+
+# test case 4 - approaches infinite limit
+print("\nPROOF 4:")
+pf = DEProof("\\lim_{x \\to -\\infty} 1 = 14")
+
