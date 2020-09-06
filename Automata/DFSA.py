3b5ab2e15bd27e5bed31eabfccc0172d08db9dd1
Iris-0829 <1648333063@qq.com>
2020-09-05 23:00:35 -0400

    added feature to show steps

--- Automata/DFSA.py
+++ Automata/DFSA.py
@@ -1,4 +1,9 @@
 class Deterministic:
+    """
+    ------------------------------------------------------------------
+    Automata.DFSA: DFSA
+    ------------------------------------------------------------------
+    """
     def __init__(self, dfsa_dict, initial, accept):
         """
         ------------------------------------------------------------------
@@ -6,7 +11,7 @@ class Deterministic:
         ------------------------------------------------------------------
         Parameters:
             dfsa_dict: Dictionary that contains the transition functon of DFSA
-                Example:
+                Example: {('q0', '1'): 'q1', ('q1', '0'): 'q0'}
         ------------------------------------------------------------------
         """
         self.dfsa_dict = dfsa_dict  # in the form (q0, str):q1
@@ -21,14 +26,40 @@ class Deterministic:
         if len(str) == 0:
             return state
 
-        y = str[:len(str) - 1]
-        a = str[-1]
+        y = str[:len(str) - 1] #state before last symbol
+        a = str[-1] # last symbol
 
-        if (self.transit(state, y), a) in self.dfsa_dict:
-            return self.dfsa_dict[(self.transit(state, y), a)]
+        y_state = (self.transit(state, y), a)
+        print('step', len(y), ': current string = ', y,
+              ', current state = ', y_state[0])
+
+        if y_state in self.dfsa_dict:
+            return self.dfsa_dict[y_state]
 
         return self.dfsa_dict[('dead', a)]
 
     def is_accepting(self, input_string):
+        result = self.transit(self.initial, input_string)
+        print('step', len(input_string), ': current string = ',
+              input_string, ', current state = ', result)
 
-        return self.transit(self.initial, input_string) in self.accept
+        return result in self.accept
+
+    def give_examples(self):
+        # give concrete examples that are accepted by DFSA
+        # not finished
+
+        result = [];
+
+        for accepted in self.accept:
+            str = ''
+            target_state = accepted
+            for key in self.dfsa_dict.keys():
+                if self.dfsa_dict[key] == target_state:
+                    str = self.dfsa_dict[key][1] + str
+                    target_state = self.dfsa_dict[key][0]
+                # to be completed
+
+        if str not in result:
+            result.append(str)
+        return result
--- Tests/test_DFSA.py
+++ Tests/test_DFSA.py
@@ -12,9 +12,9 @@ d = Deterministic({('q0', '0'): 'q1',
                    ('q3', '0'): 'q2',
                    ('q3', '1'): 'q3'}, 'q0', ['q2'])
 
-print(d.is_accepting('')) #false
-print(d.is_accepting('0')) #false
-print(d.is_accepting('1')) #false
-print(d.is_accepting('00')) #true
+# print(d.is_accepting('')) #false
+# print(d.is_accepting('0')) #false
+# print(d.is_accepting('1')) #false
+# print(d.is_accepting('00')) #true
 print(d.is_accepting('0111010')) #true
-print(d.is_accepting('100011')) #false
+# print(d.is_accepting('100011')) #false
