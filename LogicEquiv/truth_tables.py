1b30f2fae83b5c340fe8165b0c9f0b65fb5e2eda
Jan Garong <jan.garong@mail.utoronto.ca>
2020-09-07 12:45:32 -0400

    made corrections on cfg unittest

--- LogicEquiv/truth_tables.py
+++ LogicEquiv/truth_tables.py
@@ -20,6 +20,6 @@ class TruthTables:
     def __init__(self):
         self.le_not = LESymbol('\\neg', {'1': '0', '0': '1'})
         self.le_and = LESymbol('\\wedge', {'11': '1', '10': '0', '01': '0', '00': '0'})
-        self.le_and = LESymbol('\\vee', {'11': '1', '10': '1', '01': '1', '00': '0'})
+        self.le_or = LESymbol('\\vee', {'11': '1', '10': '1', '01': '1', '00': '0'})
         self.le_implies = LESymbol('\\rightarrow', {'11': '1', '10': '0', '01': '1', '00': '1'})
-        self.le_implies = LESymbol('\\leftrightarrow', {'11': '1', '10': '0', '01': '0', '00': '1'})
+        self.le_equiv = LESymbol('\\leftrightarrow', {'11': '1', '10': '0', '01': '0', '00': '1'})
